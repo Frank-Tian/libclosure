3c8efeda299a107e40915c7f2509acd45ae5379f
zhenzi <tianzhenzi@inke.cn>
2021-04-27 12:12:30 +0800

    Build Success

--- libclosure-74/BlockDemo/main.m
+++ libclosure-74/BlockDemo/main.m
@@ -7,9 +7,39 @@
 
 #import <Foundation/Foundation.h>
 
+// typedef 定义 blk_t
+typedef int (^blk_t)(int);
+
+blk_t block() {
+    return ^(int a) {
+        return a+1;
+    };
+}
+
+int callBlock(blk_t blk) {
+    return blk(333);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        
+        
+        // 1
+        void (^blk)(void) = ^{
+            NSLog(@"======== block =======");
+        };
+        blk();
+        
+        // 2
+        int (^blk2)(int) = ^int(int a) {
+            return a+1;
+        };
+        NSLog(@"%d", blk2(10));
+        
+        // 3
+        int sum = callBlock(block());
+        NSLog(@"%d", sum);
+        
         NSLog(@"Hello, World!");
     }
     return 0;
