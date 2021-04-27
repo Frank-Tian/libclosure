d731608e47416d53eca93c2e67e9fad97a942040
zhenzi <tianzhenzi@inke.cn>
2021-04-27 11:44:32 +0800

    Project Init

---
+++ libclosure-74/BlockDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockDemo
+//
+//  Created by Tian on 2021/4/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
